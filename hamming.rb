e99e8e8a20055ffce51fcac31e0fe11def219144
Arnaud VERSINI <Arnaud.VERSINI@gmail.com>
2011-06-17 09:52:53 +0200

    Création de la classe pour le codage et l'encodage de Hamming

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,58 @@
 class Hamming
 
-  def encode
+  @matrice = [
+	[1, 0, 0, 0],
+	[0, 1, 0, 0],
+	[0, 0, 1, 0],
+	[0, 0, 0, 1],
+	[1, 1, 0, 1],
+	[1, 0, 1, 1],
+	[0, 1, 1, 1]]
+
+  @matriceEncodage = [
+	0x00,
+	0x71,
+	0x62,
+	0x13,
+	0x54,
+	0x25,
+	0x36,
+	0x47,
+	0x38,
+	0x49,
+	0x5A,
+	0x2B,
+	0x6C,
+	0x1D,
+	0x0E,
+	0x7F 
+    ]
+
+  def init()
+
+  end
+  
+  #Encode une chaine de caractére contenant des 0 et des 1
+  def encodeBinStr (strToEncode)
+    
   end
 
-  def decode
+  #Decode une chaine de caractére contenant des 0 et des 1
+  def decodeBinStr (strToDecode)
+    
+  end
+
+  def encodeUnsignedByte(byteToEncode)
+  end
+
+  #Decode une chaine de caractére contenant des 0 et des 1
+  def encodeQuartet (quartetToEnccode)
+  end
+
+  #Decode une chaine de caractére contenant des 0 et des 1
+  def decodeQuartet (quartetToDecode)
+    
   end
 end
+
+hamming = Hamming.new
