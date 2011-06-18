14e4d07e7e8372b66df736454f6d9f42ebc2608c
Arnaud Versini <arnaud.versini@gmail.com>
2011-06-18 12:36:48 +0200

    Deobjectisation de Hamming.

--- testHamming.rb
+++ testHamming.rb
@@ -3,29 +3,25 @@ require "hamming"
 
 class HammingTest < Test::Unit::TestCase
   def test_encodage_decodage_quartet
-    ham = Hamming.new
     0.upto(15) do | i |
-      assert_equal(i, ham.decodeQuartet(ham.encodeQuartet(i)))
+      assert_equal(i, decode_quartet(encode_quartet(i)))
     end
   end
 
   def test_encodage_decodage_unsigned_byte
-    ham = Hamming.new
     0.upto(255) do | i |
-      assert_equal(i, ham.decodeUnsignedBytes(ham.encodeUnsignedByte(i)))
+      assert_equal(i, decode_unsigned_bytes(encode_unsigned_byte(i)))
     end
   end
 
   def test_quartet_to_byte
-    ham = Hamming.new
     0.upto(255) do | i |
-      assert_equal(i, ham.quartetsToByte(ham.unsignedByteToQuartets(i)))
+      assert_equal(i, quartets_to_byte(unsigned_byte_to_quartets(i)))
     end
   end
 
   def test_encodage_decodage_str
-    ham = Hamming.new
-    assert_equal("toto", ham.decodeBinStr(ham.encodeBinStr("toto")))
+    assert_equal("toto", decode_bin_str(encode_bin_str("toto")))
   end
 end
 
